ea2814c870cc5e4eba07eef09ab0ca864531cde5
lmirallm <lmirallm@everis.com>
2019-05-03 12:17:48 +0200

    Timer working

--- src/main/java/cars/service/CarService.java
+++ src/main/java/cars/service/CarService.java
@@ -1,19 +1,6 @@
 package cars.service;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.logging.Logger;
-
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -22,20 +9,8 @@ import cars.entity.Car;
 
 @Stateless
 public class CarService {
-	Timer timer = new Timer();
-	
-	TimerTask tarea = new TimerTask() {
-
-		@Override
-		public void run() {
-			
-		}	
-	};
-
-	private final static Logger LOGGER = Logger.getLogger(CarService.class.getName());
 	@PersistenceContext(unitName = "em_pu")
 	private EntityManager em;
-
 
 	public List<Car> getCars() {
 		return (List<Car>) em.createQuery("SELECT c FROM Car c").getResultList();
